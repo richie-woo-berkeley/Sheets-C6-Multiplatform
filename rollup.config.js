8561c100143f6d92b778ad46f1acb984bbb0e79c
J. Christopher Anderson <jcanderson2167@gmail.com>
2025-04-27 13:37:43 -0700

    minor

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,16 @@
+import json from '@rollup/plugin-json';
+
+export const C6_VERSION = '1.0.5';
+
 export default {
-    input: 'src/index.js',    // <-- your main code goes here
-    output: {
-      file: 'dist/c6-sim.min.js',
-      format: 'umd',
-      name: 'C6',
-      exports: 'default',
-      sourcemap: true
-    }
-  };
+  input: 'src/index.js',
+  output: {
+    file: 'dist/c6-sim.min.js',
+    format: 'umd',
+    name: 'C6',
+    exports: 'default',
+    sourcemap: true,
+    banner: '// C6-Sim version 1.0.5'
+  },
+  plugins: [json()]
+};
